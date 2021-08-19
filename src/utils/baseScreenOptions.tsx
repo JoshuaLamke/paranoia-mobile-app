892fd3f8e6b56c281c792925e2d29b1c99efd98c
Joshua Lamke <joshua.lamke@gmail.com>
2021-08-19 11:42:38 -0400

    added logo to center of each page

--- src/utils/baseScreenOptions.tsx
+++ src/utils/baseScreenOptions.tsx
@@ -1,5 +1,6 @@
-import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import React from 'react';
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
+import { Image } from 'react-native';
 import COLORS from './colors';
 
 const baseOptions: () => NativeStackNavigationOptions = () => {
@@ -8,7 +9,8 @@ const baseOptions: () => NativeStackNavigationOptions = () => {
         headerStyle: {backgroundColor: COLORS.primaryBackgroundLight},
         headerTitleStyle: {color: COLORS.primaryText},
         headerTitleAlign: "center",
-        headerBackTitle: "Back"
+        headerBackTitle: "Back",
+        headerTitle: () => <Image style={{width: 60, height: 60}} source={require("../../assets/p-logo.png")}/>
     })
 }
 
